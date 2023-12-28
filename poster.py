53981a92aeb7f7b10ea88652c28bd13b28b0a1ba
JanLohse <jan.lars.lohse@gmail.com>
2023-12-28 20:26:25 +0100

    Update

--- poster.py
+++ poster.py
@@ -1,13 +1,60 @@
 import svgwrite
+import csv
 import os
+import coloria
+from datetime import datetime
 
 
-def entry_to_color(artist, date, start_date, end_date):
-    pass
+# page setup
+PAGE_WIDTH = 42
+PAGE_HEIGHT = 29.7
+GRAPH_SCALE = .8
+GRAPH_WIDTH = PAGE_WIDTH * GRAPH_SCALE
+GRAPH_HEIGHT = PAGE_HEIGHT * GRAPH_SCALE
+
+# color setup
+L = .75
+C = .125
+H_MIN = 0
+H_MAX = 320
+B_COLOR = None
+
+# line setup
+DURATION_BASE = 5
+DURATION_MAX = 10
+LINE_EXPANSION = 1.3
+CORNER_RADIUS = .3
+
+
+def entry_to_date(entry):
+    return datetime.strptime(entry[1].split(",")[0], "%d %b %Y")
+
+
+def entry_to_time(entry):
+    return datetime.strptime(entry[1].split(",")[1], " %H:%M")
+
+
+def entry_to_color(entry, start_date, end_date, artists):
+    if entry[2] not in artists:
+        artists[entry[2]] = None
+    return artists[entry[2]]
+
+
+def add_entry(dwg, previous, current, next, start_date, end_date, artists):
+    color = entry_to_color(current, start_date, end_date, artists)
 
 
 def generate_poster(src):
     dwg = svgwrite.Drawing(src.split('.')[0] + '.svg')
+    artists = dict()
+
+    with open(src, "r", encoding="utf8") as file:
+        entries = [None] + list(csv.reader(file))[1:] + [None]
+        start_date = entry_to_date(entries[-2])
+        end_date = entry_to_date(entries[1])
+        for i in range(1, len(entries) - 1):
+            add_entry(dwg, entries[i-1], entries[i], entries[i+1], start_date, end_date, artists)
+            break
     dwg.save()
 
 
