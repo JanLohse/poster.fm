e772503404e6928f213c50f90dff5789dcc0e209
JanLohse <jan.lars.lohse@gmail.com>
2023-12-30 20:31:28 +0100

    Clip rgb

--- poster.py
+++ poster.py
@@ -15,15 +15,15 @@ OFFSET_X = (PAGE_WIDTH - GRAPH_WIDTH) / 2
 OFFSET_Y = (PAGE_HEIGHT - GRAPH_HEIGHT) / 2
 
 # color setup
-L = .75
+L = .66
 C = .125
 H_MIN = 0
 H_MAX = 320
 B_COLOR = None
 
 # line setup
-DURATION_BASE = 5
-DURATION_MAX = 10
+DURATION_BASE = 8
+DURATION_MAX = 16
 LINE_EXPANSION = 1.3
 CORNER_RADIUS = .3
 
@@ -44,7 +44,8 @@ def entry_to_color(entry, start_date, end_date, artists):
         h = H_MIN + (H_MAX - H_MIN) * date_progress
         a = C * math.cos(math.pi / 180 * h)
         b = C * math.sin(math.pi / 180 * h)
-        artists[entry[2]] = colour.XYZ_to_sRGB(colour.Oklab_to_XYZ([L, a, b]))
+        rgb = colour.XYZ_to_sRGB(colour.Oklab_to_XYZ([L, a, b]))
+        artists[entry[2]] = [min([255, max([0, int(i * 255)])]) for i in rgb]
     return artists[entry[2]]
 
 
@@ -66,7 +67,7 @@ def add_entry(dwg, previous, current, successor, start_date, end_date, artists):
     height = f"{GRAPH_HEIGHT * (current_end - current_start) / 1440}cm"
     # print(f"{x=} {y=} {width=} {height=}")
     dwg.add(dwg.rect(insert=(x, y), size=(width, height),
-                     fill=f"rgb({int(color[0] * 255)},{int(color[1] * 255)},{int(color[2] * 255)})"))
+                     fill=f"rgb({color[0]},{color[1]},{color[2]})"))
 
 
 def generate_poster(src):
